2d47c6a719e368605af975d97ba2e6c87aa57961
Monir <fayhadahmedmonir@gmail.com>
2024-09-08 00:09:09 +0600

    complete navbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "background-image-1":'linear-gradient(to left, #2ae1a9 30%,#7CDF6B)',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "pri-grad-1": "#538EB8",
+        "pri-grad-2": "#7CDF6B",
+        "gray-1":"#F7F7F7",
+        "gray-2":"#8D8D8D",
+        pri: "#2ae1a9",
+        
+
       },
     },
   },
